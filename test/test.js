dcc73ef74d0a4a74a9e3878306f12ec4e7fabc20
Aleksi Sapon <aleksi@sapon.ca>
2016-01-28 14:06:05 -0500

    Remove example tests

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-describe('Array', function() {
-	describe('#indexOf()', function() {
-		it('should return -1 when val is not present', function() {
-			assert.equal(-1, [1,2,3].indexOf(5));
-		});
-	});
-});
