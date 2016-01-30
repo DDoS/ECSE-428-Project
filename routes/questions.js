d9df0fd0e7d410b1931004fb246106c914a9db9b
chianghomer <chianghomer@gmail.com>
2016-01-30 15:52:26 -0500

    minor error fixed

--- routes/questions.js
+++ routes/questions.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var database = require('././data/db');
 
 router.get('/create', function(req, res) {
     res.render('questions/create', {
