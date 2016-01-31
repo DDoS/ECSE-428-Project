98368cd3ad21bf0a24e1f3ade4f7007ba7d77c72
chianghomer <chianghomer@gmail.com>
2016-01-31 02:01:01 -0500

    added backend create new question not done

--- routes/questions.js
+++ routes/questions.js
@@ -7,5 +7,30 @@ router.get('/create', function(req, res) {
     });
 });
 
-module.exports = router;
 
+router.post('/create', function(req, res){
+    if (req.user === undefined) {
+        req.flash('errors', { msg: 'Please login before posting new question.' });
+        return res.redirect('/users/login');
+
+    }else{
+        req.assert('title', 'Title is empty.').notEmpty();
+        req.assert('text', 'Description is empty.').notEmpty();
+
+        var errors = req.validationErrors();
+
+        if (errors) {
+            req.flash('errors', errors);
+            return res.redirect('/questions/create');
+        }else {
+            req.app.get('db').createQuestion(req.body.title, req.body.text, req.user.username, function (question) {
+                //console.log(question);
+                req.flash('success', {msg: 'New question posted!'});
+                res.redirect('/');
+            });
+        }
+    }
+
+});
+
+module.exports = router;
