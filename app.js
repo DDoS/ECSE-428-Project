890ba0dc7e5d3ea125cca9080b4dc24598dce639
Ze-Q <ze.q.zhang@mail.mcgill.ca>
2016-01-30 20:29:09 -0500

    Login backend and frontend done, missing tests

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var db = require('./data/db');
 var flash = require('express-flash');
 var expressValidator = require('express-validator');
 var session = require('express-session');
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var db = require('./data/db');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var arguments = require('./routes/arguments');
 var questions = require('./routes/questions');
 
-const dbName = 'mayhem';
+var passportConf = require('./config/passport');
 
 var app = express();
 
@@ -35,6 +36,12 @@ app.use(session({
 app.use(flash());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next) {
+    res.locals.user = req.user;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
@@ -73,7 +80,7 @@ app.use(function(err, req, res, next) {
 });
 
 // Create and initialize the DB
-app.database = new db.Database(dbName);
+app.database = db.Database;
 app.database.initialize(function() {
     console.log("Database is ready");
 });
