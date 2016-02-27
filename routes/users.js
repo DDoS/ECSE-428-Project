f53b066e6f302ded45f8911e82e24e539d53514d
Michael Kourlas <michael@kourlas.net>
2016-02-27 12:40:09 -0500

    Minor refactoring to fix cucumber tests

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ router.get('/login', function(req, res) {
 
 router.post('/login', function(req, res, next) {
   req.assert('username', 'Username cannot be empty.').notEmpty();
-  req.assert('password', 'Password cannot be blank.').notEmpty();
+  req.assert('password', 'Password cannot be empty.').notEmpty();
 
   var errors = req.validationErrors();
 
@@ -31,8 +31,8 @@ router.post('/login', function(req, res, next) {
       if (err) {
         return next(err);
       }
-      req.flash('success', { msg: 'Success! You are logged in.' });
-      res.redirect('/');
+      req.flash('success', { msg: 'You are now logged in.' });
+      res.redirect('/questions/find');
     });
   })(req, res, next);
 });
@@ -44,9 +44,9 @@ router.get('/create', function(req, res) {
 });
 
 router.post('/create', function(req, res, next) {
-  req.assert('username', 'User name is empty.').notEmpty();
-  req.assert('email', 'Email is not valid.').isEmail();
-  req.assert('password', 'Password must be at least 4 characters long.').len(4);
+  req.assert('username', 'Username is empty.').notEmpty();
+  req.assert('email', 'Email is invalid.').isEmail();
+  req.assert('password', 'Password must be at least 4 characters in length.').len(4);
   req.assert('confirmPassword', 'Passwords do not match.').equals(req.body.password);
 
   var errors = req.validationErrors();
@@ -60,11 +60,11 @@ router.post('/create', function(req, res, next) {
     if (user === undefined) { //if username is not already taken
       req.app.get('db').createUser(req.body.username, req.body.password, req.body.email, function(user) {
         //console.log(user);
-        req.flash('success', { msg: 'Account created successfully, please login.' });
+        req.flash('success', { msg: 'Account successfully created.' });
         res.redirect('/users/login');
       });
     } else { //username already taken
-      req.flash('errors', { msg: 'Account with that user name already exists.' });
+      req.flash('errors', { msg: 'Account with that username already exists.' });
       return res.redirect('/users/create');
     }
   });
