c0c018d26e06981534f8e0345f885cecbefd17ba
Michael Kourlas <michael@kourlas.net>
2016-03-07 18:47:45 -0500

    Added front-end and back-end support for edit account email/password

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+var async = require('async');
+
 var express = require('express');
 var passport = require('passport');
 var router = express.Router();
@@ -74,5 +76,94 @@ router.get('/logout', function(req, res) {
   req.logout();
   res.redirect('/');
 });
+
+router.get('/account', isAuthenticated, function(req, res) {
+  var database = req.app.get('db');
+
+  async.waterfall([
+    function(done) {
+      // Retrieve account information from database
+      try {
+        database.getUser(req.user.username, function(user) {
+          done(undefined, user);
+        });
+      } catch(err) {
+        done(err);
+      }
+    },
+    function(user, done) {
+      // Display account information
+      res.render('users/account', {
+        title: 'Manage Account',
+        email: user.email
+      });
+      done();
+    }
+  ]);
+});
+
+router.post('/account', isAuthenticated, function(req, res) {
+  var database = req.app.get('db');
+
+  // Client input validation
+  if (req.body.type === 'update') {
+    req.assert('email', 'Email is invalid.').isEmail();
+    req.assert('password', 'Password must be at least 4 characters in length.').len(4);
+    req.assert('confirmPassword', 'Passwords do not match.').equals(req.body.password);
+  } else if (req.body.type === 'delete') {
+    // TODO: Input validation for account deletion
+  } else {
+    return error();
+  }
+  var errors = req.validationErrors();
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/users/account');
+  }
+
+  if (req.body.type === 'update') {
+    async.series([
+      function(done) {
+        try {
+          database.setUser(req.user.username, {
+            email: req.body.email,
+            password: req.body.password
+          }, function() {
+            done();
+          });
+        } catch (err) {
+          done(err);
+        }
+      },
+      function(done){
+        req.flash('success', {
+          msg: 'Account information updated.'
+        });
+        res.redirect(req.get('referer'));
+      }
+    ], function(err) {
+      if (err) {
+        error();
+      }
+    });
+  } else {
+    // TODO: Account deletion
+  }
+
+  function error() {
+    req.flash('errors', {
+      msg: 'Failed to update account information. Please try again.'
+    });
+    res.redirect(req.get('referer'));
+  }
+});
+
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.flash('errors', {msg: 'Please login before performing that action.'});
+    res.redirect('/users/login');
+}
 
 module.exports = router;
