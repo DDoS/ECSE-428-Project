ad9d812b1759c1f5856e630de70214ce01a904ac
Ze-Q <ze.q.zhang@mail.mcgill.ca>
2016-03-20 22:02:00 -0400

    Revert Harry's commits

--- test-cucumber/config.js
+++ test-cucumber/config.js
@@ -53,7 +53,7 @@ exports.config = {
      * - baseUrl: sets base url for `Given I open the site "/some/url.html"`
      */
     env: {
-        baseUrl: 'localhost:8080'
+        baseUrl: 'http://127.0.0.1:8080'
     },
 
     /**
--- test-cucumber/support/custom/editArgument.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(id, questionId, done) {
-    this.browser
-        .url(this.baseUrl + '/arguments/edit?q=#' + this.questions[questionId].id + '&a=#' + this.arguments[id].id)
-        .call(done);
-};
